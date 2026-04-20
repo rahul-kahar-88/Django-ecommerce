cb2f7087ca48b6c33b3651e3dd914d1e1a360bd7
rahul kahar 88 <rahulkahar88588@gmail.com>
2026-04-20 06:30:31 +0530

    update

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2@ig6r&dvo4wusfsiaw_3b^6e(0+u!^+b593**s(2dwbzg6xs3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1' , 'rahulkahar.pythonanywhere.com' ]
 
 
 # Application definition
