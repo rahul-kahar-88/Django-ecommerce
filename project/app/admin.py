09636670468a4dbb7105797cfa623223009d8be3
rahul kahar 88 <rahulkahar88588@gmail.com>
2026-06-09 22:35:52 +0530

    update

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Department, Add_Employee, Query, Item, Order, Cart
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Department)
+admin.site.register(Add_Employee)
+admin.site.register(Query)
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(Cart)
